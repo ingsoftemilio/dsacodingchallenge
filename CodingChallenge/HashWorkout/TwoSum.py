c6ef78bd51e5f48185f58ea484f5e3c17bea8f0b
ingsoftemilio <ingsoftemilio@gmail.com>
2024-11-06 19:57:35 -0500

    TwoSum corrected

--- CodingChallenge/HashWorkout/TwoSum.py
+++ CodingChallenge/HashWorkout/TwoSum.py
@@ -16,30 +16,26 @@ def two_sum(arr_nums,target):
 
     for index,num_current in enumerate(arr_nums):
 
-        # Just do this if the count elements of the array is 2 or more because with 1 or less I coulndt find a difference
-        if len(arr_nums)>1:
-            print(f"index: {index}")
-            print(f"num_current: {num_current}")
 
-            # GET DIFFERENCE TARGET FROM MY CURRENT VALUE
-            difference = target-num_current
-            print(f"difference: {difference}")
+        print(f"index: {index}")
+        print(f"num_current: {num_current}")
 
-            # KEEP A TRACK OF THE DIFFERENCES AND ITS INDEX
-            container_differences_dict[difference]=index
+        # GET DIFFERENCE TARGET FROM MY CURRENT VALUE
+        difference = target-num_current
+        print(f"difference: {difference}")
 
-            # If the index is the second elemtn or more and If the difference is in my dictionary, return the index where the difference exists and return my current index
-            if(index>0):
-                if num_current in container_differences_dict:
-                    print(f"the current number {num_current} is found in my dictionary of differences")
-                    return [container_differences_dict[num_current],index]
-            
-            
+        # If the difference is in my dictionary, return the index where the difference exists and return my current index
+        if num_current in container_differences_dict:
+            print(f"the current number {num_current} is found in my dictionary of differences")
+            return [container_differences_dict[num_current],index]
+
+        # KEEP A TRACK OF THE DIFFERENCES AND ITS INDEX
+        container_differences_dict[difference]=index
 
         print(container_differences_dict)
         print("")
 
-target=18
+target=22
 arr_nums = [2, 7, 11, 15]    
 res = two_sum(arr_nums,target)
 print("***")
